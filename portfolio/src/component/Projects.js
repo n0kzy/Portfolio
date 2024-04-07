493c019b6c38cc6a39c9df1a91656ada32445227
PATRICK <PATRICK@PATRICKPC>
2024-04-07 15:03:18 +0200

    surement le dernier push

--- portfolio/src/component/Projects.js
+++ portfolio/src/component/Projects.js
@@ -22,22 +22,11 @@ import Popup from "reactjs-popup";
                         {modal && (
                             <div className="middle">
                                 <span className="close" onClick={toggleModal}>&times;</span>
-                                <div className="text">Dans le cadre de mes Saé de 175 heures, j'ai développé un projet
-                                    pour
-                                    améliorer les compétences linguistiques en anglais, incluant des quiz, jeux de
-                                    mémoire
-                                    et mode examen. Nous avons (avec mes 3 coéquipiers) optimisé la base de données pour
-                                    garantir la sécurité des données. Grâce à une démarche agile, nous avons assuré la
-                                    satisfaction des parties prenantes en répondant aux besoins métiers. J'ai collaboré
-                                    avec
-                                    mon équipe pour situer mon rôle et contribuer à la réalisation des objectifs. Les
-                                    langages PHP, Android, JavaScript et EntityFramework (.NET) ont été utilisés. Nous
-                                    avons
-                                    suivi la méthode Scrum et utilisé des tableaux Kanban pour organiser notre travail
-                                    en
-                                    équipe. Cette expérience a enrichi mes compétences techniques et organisationnelles,
-                                    tout en contribuant à l'amélioration des compétences linguistiques des
-                                    utilisateurs. <br/><br/> <a
+                                <div className="text">Pendant mes 175 heures de projet SAÉ, nous avons collaboré à une initiative visant à renforcer les compétences linguistiques en anglais à travers des quiz, des jeux de mémoire et un mode examen. Notre équipe, composée de quatre membres, a œuvré ensemble pour optimiser la sécurité des données en améliorant la base de données. Grâce à une approche agile, nous avons répondu de manière efficace aux besoins métiers et veillé à la satisfaction des parties prenantes. J'ai joué un rôle actif dans l'équipe en contribuant à la définition des rôles et à la réalisation des objectifs communs. Nos compétences en langages de programmation, notamment PHP, Android, JavaScript et EntityFramework (.NET), ainsi que notre utilisation de la méthodologie Scrum et des tableaux Kanban, ont facilité notre travail collaboratif.
+
+                                    Parallèlement, j'ai assumé la responsabilité individuelle du développement de l'API utilisée dans l'application, en tant que seul membre ayant suivi la spécialité mobile. J'ai travaillé à partir de la base de données que nous avions élaborée en groupe plus tôt dans l'année pour créer une API complète, que j'ai ensuite déployée grâce à Docker. J'ai également ajouté un jeton d'authentification (bearer token) pour renforcer la sécurité de l'application.
+
+                                    Cette expérience a été enrichissante à bien des égards, tant sur le plan technique que sur le plan organisationnel, et a également contribué à l'amélioration des compétences linguistiques des utilisateurs.<br/><br/> <a
                                         href="https://codefirst.iut.uca.fr/git/antoine.jourdain/sae_2a_anglais">REPOS</a>
                                 </div>
                             </div>
@@ -49,29 +38,28 @@ import Popup from "reactjs-popup";
                         { modal2 && (
                             <div className="middle">
                                 <span className="close" onClick={toggleModal2}>&times;</span>
-                                <div className="text">Dans le cadre de mes Saé de 175 heures, j'ai réalisé seul un
-                                    projet en
-                                    MAUI,
-                                    une application fournissant une large gamme d'informations sur les jeux vidéo.
-                                    Malgré
-                                    les
-                                    imprévus
-                                    dus à l'absence de mon coéquipier, j'ai su m'adapter et mener à bien le projet.
-                                    Cette
-                                    application
-                                    offre des détails sur les personnages (champions), les cartes et les modes de jeu
-                                    disponibles,
-                                    comme illustré dans l'image fournie. J'ai optimisé la base de données pour gérer
-                                    efficacement
-                                    les vastes quantités d'informations sur les jeux vidéo. Malgré les défis, j'ai
-                                    appliqué
-                                    une
-                                    démarche de suivi de projet en utilisant des kanbans. Enfin, j'ai assumé le rôle
-                                    complet
-                                    de
-                                    l'équipe pour mener à bien le projet, démontrant ma capacité à travailler de manière
-                                    autonome
-                                    et à m'adapter aux circonstances imprévues. <br/><br/>
+                                <div className="text">Dans le cadre de mes SAÉ de 175 heures, j'ai réalisé seul un projet MAUI,
+                                    une application fournissant une large gamme d'informations sur les jeux vidéo. Malgré les
+                                    imprévus dus à l'absence de mon coéquipier, j'ai su m'adapter et mener à bien le projet.
+                                    Cette application offre des détails sur les personnages (champions), les cartes et les modes
+                                    de jeu disponibles, comme illustré dans l'image fournie. J'ai optimisé la base de données
+                                    pour gérer efficacement les vastes quantités d'informations sur les jeux vidéo. Malgré les
+                                    défis, j'ai appliqué une démarche de suivi de projet en utilisant des kanbans. Enfin, j'ai
+                                    assumé le rôle complet de l'équipe pour mener à bien le projet, démontrant ma capacité à
+                                    travailler de manière autonome et à m'adapter aux circonstances imprévues.
+
+                                    Dans le cadre de la conception de ce projet MAUI, j'ai également été amené à réaliser trois
+                                    diagrammes essentiels avant la phase de développement. Tout d'abord, j'ai conçu un diagramme
+                                    de classe pour représenter la structure des données et des objets de l'application.
+                                    Ensuite, j'ai élaboré un autre diagramme de classe mettant en avant la partie persistance,
+                                    détaillant la manière dont les données seraient stockées et récupérées dans la base de données.
+                                    Enfin, j'ai créé un diagramme de paquetage pour organiser et visualiser la structure globale de
+                                    l'application. Concernant l'architecture du projet, j'ai fait le choix d'adopter une architecture
+                                    en couches, avec une séparation claire entre la présentation, la logique métier et la persistance
+                                    des données.
+                                    J'ai également élaboré des user stories, des esquisses et des storyboards pour visualiser et planifier
+                                    les fonctionnalités de l'application, en assurant une cohérence entre les différentes parties du projet.
+                                    Toutes ses réalisations sont disponibles dans le dépot ci-dessous.<br/><br/>
                                     <a
                                         href="https://codefirst.iut.uca.fr/git/alexandre.bouvot/SAE_2.01_BOUVOT_BRUGIERE.git">REPOS</a>
                                 </div>
